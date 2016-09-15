b5cf02fc0caf4038d8f7604051111fef1d916b49
2dama <2dama@PC221019.informatica.edu>
2016-09-15 17:09:56 +0200

    Proyecto inicial sin modificaciones

---
+++ src/ej1_dad/EJ1_DAD.java
@@ -0,0 +1,44 @@
+
+package ej1_dad;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class EJ1_DAD 
+{
+    public static void main(String[] args) 
+    {
+        //-----------------
+        JFrame marco = new JFrame ("Ejercicio 1 - DAD");
+        
+        JLabel etiqueta_a = new JLabel ("Primera etiqueta");
+        JLabel etiqueta_b = new JLabel ("Segunda etiqueta");
+        
+        JTextField campo_a = new JTextField ("Primer campo de texto");
+        JTextField campo_b = new JTextField ("Segundo campo de texto");
+        
+        JButton boton = new JButton ("Botón");
+        
+        FlowLayout layout = new FlowLayout();
+        
+        marco.setVisible(true);
+        marco.setSize(600,500);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setLayout(layout);
+        
+        marco.getContentPane().add(etiqueta_a);
+        marco.getContentPane().add(etiqueta_b);
+        marco.getContentPane().add(campo_a);
+        marco.getContentPane().add(campo_b);
+        marco.getContentPane().add(boton);
+        
+        marco.pack();
+    }
+}
